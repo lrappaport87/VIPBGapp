5e8c98a840c46c0f914b210f1189ead10d1b2fd2
Lance Rappaport <lmrappaport@vipbgb1l01130db.local>
2016-09-16 17:48:44 -0400

    Initial Commit

---
+++ VIPBGapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VIPBGapp
+//
+//  Created by Lance Rappaport on 9/16/16.
+//  Copyright © 2016 Lance Rappaport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
